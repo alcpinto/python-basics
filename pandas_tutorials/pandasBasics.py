12a2e83a368a90a1b719a7ce5dccfaddd15f44dc
alcpinto <abilio.pinto@sessioncam.com>
2017-03-09 13:20:13 +0000

    Pandas

---
+++ pandas_tutorials/pandasBasics.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
+
+web_stats = {'Day': [1, 2, 3, 4, 5, 6],
+             'Visitors': [43, 34, 65, 56, 29, 76],
+             'Bounce Rate': [65, 67, 78, 65, 45, 52]}
+
+df = pd.DataFrame(web_stats)
+
+# returns a new dataframe
+df2 = df.set_index('Day')
+print(df)
+
+# change existing dataframe
+# df.set_index('Day', inplace=True)
+# print(df)
+
+
+# print(df)
+# print(df.head())
+# print(df.head(3))
+# print(df.tail())
+# print(df.tail(2))
+
+# similar statements to refer columns
+# columns names with spaces are only allowed with first syntax
+print(df['Visitors'])
+print(df.Visitors)
+
+# refer several columns
+print(df[['Visitors', 'Bounce Rate']])
+
+#  convert result to list
+print(df.Visitors.tolist())
+#  convert result to array if multi columns
+print(np.array(df[['Visitors', 'Bounce Rate']]))
+
+df3 = pd.DataFrame(np.array(df[['Visitors', 'Bounce Rate']]))
+print(df3)
+
---
+++ pandas_tutorials/pandasIntro.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import datetime
+from pandas_datareader import data as web
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2015, 8, 22)
+
+df = web.DataReader("XOM", "yahoo", start, end)
+print(df)
+print(df.head())
+
+df['High'].plot()
+plt.legend()
+plt.show()
