7e0da4ee155a1f91b4e2f66a364781eba8258b5c
alcpinto <abiliopinto@gmail.com>
2017-03-23 07:23:37 +0000

    data analysis

--- pandas_tutorials/scikitMachineLearning.py
+++ pandas_tutorials/scikitMachineLearning.py
@@ -45,4 +45,4 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 clf = svm.SVC(kernel='linear')
 clf.fit(X_train, y_train)
 
-# print(clf.score(X_test, y_test))
+print(clf.score(X_test, y_test))
